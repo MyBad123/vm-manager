a0db9a0f33ab243f1d1cf97c114ae73fc5dbeae6
MyBad1231 <genag4448@yandex.ru>
2025-03-23 22:50:02 +0300

    feat (client.py): final for client

--- src/client.py
+++ src/client.py
@@ -2,11 +2,77 @@ import asyncio
 import json
 
 
+def get_user_input(message, is_int=False):
+    """A function for receiving user input with an integer check."""
+    
+    while True:
+        user_input = input(message)
+        if is_int and user_input.isdigit():
+            return int(user_input)
+        elif not is_int:
+            return user_input
+        else:
+            print("Ошибка! Пожалуйста, введите целое число.")
+
+
+async def handle_create_vm():
+    """Processing the creation of a VM."""
+
+    ram_size = get_user_input("Введите количество памяти: ", True)
+    cpu_count = get_user_input("Введите число ядер: ", True)
+    
+    login = get_user_input("Введите имя пользователя: ")
+    password = get_user_input("Введите пароль: ")
+    
+    disks = input("Введите размер дисков через пробел: ")
+    try:
+        disks = list(map(int, disks.split()))
+    except ValueError:
+        print("Ошибка! Пожалуйста, введите только целые числа через пробел.")
+        return None
+
+    return {
+        'ram_size': ram_size,
+        'cpu_count': cpu_count,
+        'login': login,
+        'password': password,
+        'disks': disks
+    }
+
+
+async def handle_update_vm():
+    """Processing the VM update."""
+
+    vm_id = get_user_input("Введите id виртуальной машины: ", True)
+    data = {'id': vm_id}
+
+    # Обновление параметров
+    if get_user_input("Хотите обновить память? (yes/no): ").lower() == 'yes':
+        data['ram_size'] = get_user_input("Введите количество памяти: ", True)
+
+    if get_user_input("Хотите обновить количество ядер? (yes/no): ").lower() == 'yes':
+        data['cpu_count'] = get_user_input("Введите число ядер: ", True)
+
+    if get_user_input("Хотите обновить текущий логин? (yes/no): ").lower() == 'yes':
+        data['login'] = get_user_input("Введите имя пользователя: ")
+        data['password'] = get_user_input("Введите пароль: ")
+
+    if get_user_input("Хотите обновить размер дисков? (yes/no): ").lower() == 'yes':
+        disks = input("Введите размер дисков через пробел: ")
+        try:
+            data['disks'] = list(map(int, disks.split()))
+        except ValueError:
+            print("Ошибка! Пожалуйста, введите только целые числа через пробел.")
+            return None
+
+    return data
+
+
 async def send_message():
-    print("CREATE_VM - созвать виртуальную машину")
+    print("CREATE_VM - создать виртуальную машину")
     print("UPDATE_VM - обновить виртуальную машину")
     print("USED_NOW_VM - используемые виртуальные машины в текущий момент времени")
-    print("USED_VM - используемые виртуальные машины когда-либо")
+    print("USED_VM - использованные виртуальные машины когда-либо")
     print("ALL_VM - все виртуальные машины")
     print("LOGIN - подключиться к виртуальной машине")
     print("LOG_OUT - отключиться от виртуальной машины")
@@ -18,105 +84,41 @@ async def send_message():
     while True:
         is_response = False
         is_response_disk = False
-        message = input("\n\nВведите сообщение: ")
+        message = input("\n\nВведите сообщение: ").upper()
 
-        if message.upper() == 'LOGIN':
-            login = input("Введите имя пользователя: ")
-            password = input("Введите пароль: ")
-            vm_id = input("Введите ID виртуальной машины: ")
+        if message == 'LOGIN':
+            login = get_user_input("Введите имя пользователя: ")
+            password = get_user_input("Введите пароль: ")
+            vm_id = get_user_input("Введите ID виртуальной машины: ", True)
 
             data = {
                 'login': login,
                 'password': password,
-                'vm_id': int(vm_id)
+                'vm_id': vm_id
             }
-
             message = f"LOGIN {json.dumps(data)}"
 
-        elif message.upper() == 'CREATE_VM':
-            ram_size = input("Введите количество памяти: ")
-            cpu_count = input("Введите число ядер: ")
-
-            if not ram_size.isdigit() or not cpu_count.isdigit():
-                print('введите целочисленные значения для ядер или памяти')
+        elif message == 'CREATE_VM':
+            data = await handle_create_vm()
+            if data:
+                message = f"CREATE_VM {json.dumps(data)}"
+            else:
                 continue
 
-            # block with credentials
-            login = input("Введите имя пользователя: ")
-            password = input("Введите пароль: ")
-
-            # block with disk
-            disks = input("Введите размер дисков через пробел: ")
-            try:
-                disks = list(map(int, disks.split()))
-            except ValueError:
-                print("Ошибка! Пожалуйста, введите только целые числа через пробел.")
+        elif message == 'UPDATE_VM':
+            data = await handle_update_vm()
+            if data:
+                message = f"UPDATE_VM {json.dumps(data)}"
+            else:
                 continue
 
-            data = {
-                'ram_size': int(ram_size),
-                'cpu_count': int(cpu_count),
-                'login': login,
-                'password': password,
-                'disks': disks
-            }
-
-            message = f"CREATE_VM {json.dumps(data)}"
-
-        elif message.upper() == 'UPDATE_VM':
-            data = {}
-
-            id_vm = input(f"Введите id виртуальной машины: ")
-            if not id_vm.isdigit():
-                print('введите целочисленное id для виртуальной машины')
-                continue
-            data['id'] = int(id_vm)
-
-            # Обновление памяти
-            update_ram = input(f"Хотите обновить память? (yes/no): ")
-            if update_ram.lower() == 'yes':
-                ram_size = input("Введите количество памяти: ")
-                if not ram_size.isdigit():
-                    print('введите целочисленное значение для памяти')
-                    continue
-                data['ram_size'] = int(ram_size)
-
-            # Обновление числа ядер
-            update_cpu = input(f"Хотите обновить текущее количество ядер? (yes/no): ")
-            if update_cpu.lower() == 'yes':
-                cpu_count = input("Введите число ядер: ")
-                if not cpu_count.isdigit():
-                    print('введите целочисленное значение для ядер')
-                    continue
-                data['cpu_count'] = int(cpu_count)
-
-            # Обновление логина и пароля
-            update_credentials = input(f"Хотите обновить текущий логин? (yes/no): ")
-            if update_credentials.lower() == 'yes':
-                login = input("Введите имя пользователя: ")
-                password = input("Введите пароль: ")
-                data['login'] = login
-                data['password'] = password
-
-            # Обновление дисков
-            update_disks = input(f"Хотите обновить размер дисков? (yes/no): ")
-            if update_disks.lower() == 'yes':
-                disks = input("Введите размер дисков через пробел: ")
-                try:
-                    data['disks'] = list(map(int, disks.split()))
-                except ValueError:
-                    print("Ошибка! Пожалуйста, введите только целые числа через пробел.")
-                    continue
-
-            message = f"UPDATE_VM {json.dumps(data)}"
-
-        elif message.upper() in ('USED_NOW_VM', 'USED_VM', 'ALL_VM'):
+        elif message in ('USED_NOW_VM', 'USED_VM', 'ALL_VM'):
             is_response = True
 
-        elif message.upper() in ('ALL_DISKS', ):
+        elif message == 'ALL_DISKS':
             is_response_disk = True
 
-        # send data
+        # Sending data
         length = len(message.encode())
         writer.write(length.to_bytes(4, byteorder='big'))
         await writer.drain()
@@ -124,10 +126,9 @@ async def send_message():
         writer.write(message.encode())
         await writer.drain()
 
-        # get data
+        # Getting data
         size_data = await reader.read(4)
         data_size = int.from_bytes(size_data, byteorder='big')
-
         data = await reader.read(data_size)
 
         if is_response:
@@ -136,7 +137,7 @@ async def send_message():
                 print(f"  Оперативная память: {vm['ram_size']} ГБ")
                 print(f"  Количество процессоров: {vm['cpu_count']}")
                 print(f"  Размеры дисков: {', '.join(map(str, vm['disk_sizes']))}\n")
-
+            
             continue
 
         if is_response_disk:
@@ -146,12 +147,12 @@ async def send_message():
                       f"Disk Size: {disk['disk_size']} GB\n"
                       f"RAM Size: {disk['ram_size']} GB\n"
                       f"CPU Count: {disk['cpu_count']}")
-                
+            
             continue
 
         print(f"Ответ от сервера: {data.decode()}")
 
-        if message.lower() == 'exit':
+        if message == 'EXIT':
             break
 
     print("Закрытие соединения с сервером")
